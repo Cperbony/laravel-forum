ec8a9d281b54ab804e4ec344c56aa8804b032762
Claudinei Perboni <cperbony@gmail.com>
2018-10-08 12:34:44 -0300

    Threads tem respostas

---
+++ resources/views/threads/reply.blade.php
@@ -0,0 +1,14 @@
+<div class="panel panel-default">
+    <hr>
+    <div class="panel-heading">
+        <a href="#">
+            {{ $reply->owner->name }}
+        </a>
+        said {{ $reply->created_at->diffForHumans() }}
+    </div>
+
+
+    <div class="panel-body">
+        {{ $reply->body }}
+    </div>
+</div>
